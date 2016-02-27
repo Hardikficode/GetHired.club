9facca7b4d5094371f44acadc3e4bb8835829fb9
huhx0015 <mhuh@yoonhuh.com>
2016-02-26 19:12:50 -0800

    Initial commit.

---
+++ app/src/test/java/huhx0015/interview/club/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package huhx0015.interview.club;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
